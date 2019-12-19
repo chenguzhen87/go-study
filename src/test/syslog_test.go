48234bf9ff054cfd4713f1cf269c0ecf655b863a
chenzhen <546369005@qq.com>
2019-12-19 19:44:33 +0800

    'syslog功能'

--- src/test/syslog_test.go
+++ src/test/syslog_test.go
@@ -7,10 +7,9 @@ import (
 )
 
 func TestSysLog(t *testing.T) {
-	err := syslog.Dial("", "",syslog.LOG_ERR, "Saturday")
+	sysLog, err := syslog.Dial("", "",syslog.LOG_ERR, "Saturday")
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	sysLog.Emerg("Hello world!")
 }
